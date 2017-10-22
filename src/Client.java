0fccd553bad5ac4ff6e4655fd0849e3759ad1225
JuhaVuorenalho <jvuore16@student.aau.dk>
2017-10-22 13:19:32 +0200

    Commit

--- src/Client.java
+++ src/Client.java
@@ -3,8 +3,12 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		Socket clientSocket;
+		
+		
 		try {
 			
+			clientSocket = new Socket ("127.0.0.1", 5000);	
 		Socket s = new Socket("127.0.0.1", 3000);
 		
 			} catch (Exception e) {}
--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,18 @@
-import java.net.*;
-
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
 	public static void main(String[] args) {
 		
+		
 		try {
-		ServerSocket server = new ServerSocket(3000);
+			
+		serverSocket = new ServerSocket(3000);
 		Socket s = server.accept();
 		
 		
