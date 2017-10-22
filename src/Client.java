06e0c085afd371263ec6268ed82b16dabf8eb9a7
Søren Skouv <soerenskouv@gmail.com>
2017-10-22 16:07:45 +0200

    Made the beginning of a very cool client! :D

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,33 @@
 import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 //So is this
 public class Client
 {
 	public static void main(String[] args)
 	{
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("---------------------------------------------------------------");
 		System.out.println("Welcome to HangBro! Type \"start\" if you want to make a server or \"connect\" if you have friends :)");
+		System.out.println("---------------------------------------------------------------");
+		
+		String s = "";
+		
+		try {
+			s = input.readLine();
+		} catch (IOException e1) {}
+		
+		switch(s)
+		{
+			case "start": System.out.println("Starting the server!");
+				break;
+			case "connect": System.out.println("Write the ip you want to connect to");
+				break;
+			default: System.out.println("Wrong command!");
+		}
 		
 		try
 		{
@@ -13,6 +35,6 @@ public class Client
 			System.out.println("Connected to server");
 		} catch (Exception e) {}
 		
-		System.out.println("I'm the client!");
+		System.out.println("Failed to connect or the connection was closed");
 	}
 }
