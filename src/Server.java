bfd09932182baa54b7dfd0daef099905b533f2ee
Søren Skouv <soerenskouv@gmail.com>
2017-10-25 16:25:13 +0200

    trying to resolve an annoying conflict

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,9 @@
 import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.SocketHandler;
 
 //This is a testing line
 //Push it
@@ -9,13 +12,18 @@ public class Server
 {
 	public static void main(String[] args)
 	{
+		ExecutorService executor = Executors.newSingleThreadExecutor();;
+	
 		try
 		{
 			ServerSocket serverSocket = new ServerSocket(3000);
 			System.out.println("IP address: " + Inet4Address.getLocalHost().getHostAddress());  //The IP address user connected to
 			
+			    //executor.submit(new SocketHandler(serverSocket.accept()));
+			//Socket s = serverSocket.accept();
 			Socket s = serverSocket.accept();
-		
+			
+			
 			serverSocket.close();
 		} catch (Exception e) {}
 		
